f82cec6ffc2b8c3f9017ecfc7ce16984b7d8d587
stackos <stackos@qq.com>
2018-04-04 03:04:15 +0800

    command line input.

--- project/server/Logger.js
+++ project/server/Logger.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 
 class Logger {
   constructor() {
+    this.rl = null
+
     if (fs.existsSync('logs') == false) {
       fs.mkdirSync('logs')
     }
@@ -11,8 +13,12 @@ class Logger {
   log(tag, str) {
     const log = Time.NowString() + '[' + tag + ']: ' + str
 
-    console.log(log)
     fs.writeSync(this.file, log + '\r\n', null, 'utf8')
+    console.log(log)
+
+    if (this.rl) {
+      this.rl.prompt()
+    }
   }
 }
 
--- project/server/Server.js
+++ project/server/Server.js
@@ -1,10 +1,12 @@
 require('./Time')
 require('./Logger')
 const http = require('http')
+const readline = require('readline')
 const Router = require('./Router')
 const Message = require('./Message')
-const controller = require('./controller')
 const DB = require('./DB')
+const Game = require('./Game')
+const controller = require('./controller')
 
 const TAG = 'Server'
 
@@ -17,16 +19,51 @@ class Server {
     this.router.add('/login', controller.login)
 
     this.db = new DB()
+    this.game = new Game()
+
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+    this.rl.prompt()
+    Logger.rl = this.rl
+
+    this.rl.on('line', (line) => {
+      switch (line) {
+        case 'shutdown':
+          this.rl.close()
+          break
+        default:
+          this.rl.prompt()
+          break
+      }
+    }).on('close', () => {
+      this.rl = null
+      Logger.rl = null
+
+      this.shutdown()
+    })
   }
 
-  start() {
-    http.createServer((request, response) => {
+  startup() {
+    this.server = http.createServer((request, response) => {
       this.router.route(this, new Message(request, response))
     }).listen(this.port)
 
     Logger.log(TAG, 'Server running at port ' + this.port)
   }
+
+  shutdown() {
+    Logger.log(TAG, 'Server closing')
+
+    this.server.close(() => {
+      this.server = null
+      Logger.log(TAG, 'Server closed')
+
+      process.exit(0)
+    })
+  }
 }
 
 const server = new Server(80)
-server.start()
+server.startup()
